57796fd343b6406c3cb8223f43a781554bda738f
Rabbi Hossen <joyahmedauthor@gmail.com>
2024-11-03 03:01:36 +0600

    add curl options

--- bin/gen.dart
+++ bin/gen.dart
@@ -1,4 +1,6 @@
-// bin/gen.dart
+import 'dart:convert';
+import 'dart:io';
+
 void main(List<String> arguments) {
   if (arguments.isNotEmpty) {
     switch (arguments[0]) {
@@ -10,6 +12,7 @@ void main(List<String> arguments) {
         break;
       case 'another-command':
         print("Another command executed");
+        _executeCurlCommand();
         break;
       default:
         print(
@@ -19,4 +22,26 @@ void main(List<String> arguments) {
     print(
         "No command provided. Available commands: auth-api-init, multi-language, another-command.");
   }
+}
+
+void _executeCurlCommand() async {
+  // Execute the curl command
+  final process = await Process.start('bash', [
+    '-c',
+    'curl -sSL https://raw.githubusercontent.com/rabbihossenjoy/gen/main/remove_unused_constants.sh | bash'
+  ]);
+
+  // Capture the output
+  process.stdout.transform(utf8.decoder).forEach((line) {
+    print(line); // Print each line of output
+  });
+
+  // Capture errors, if any
+  process.stderr.transform(utf8.decoder).forEach((line) {
+    print("Error: $line");
+  });
+
+  // Wait for the process to exit
+  final exitCode = await process.exitCode;
+  print('Process exited with code: $exitCode');
 }
